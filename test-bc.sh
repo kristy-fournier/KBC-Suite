c5dd291cd3bf786d5eb9df6cde5867e62d9b558e
Kristy F <124598538+kristy-fournier@users.noreply.github.com>
2026-04-21 20:54:01 -0400

    Okay now i think the tests are fixed

--- test-bc.sh
+++ test-bc.sh
@@ -9,8 +9,8 @@ NC='\033[0m' # No Color
 make clean
 make DEBUG=0
 echo "This is a top secret message for KBC!" > original.txt
-./file.out ./original.txt enc cbc ./middle.kbc "AAAA" "BBBB"
-./file.out ./middle.kbc dec cbc ./decrypt.txt "AAAA" "BBBB"
+./file-kbc.out ./original.txt enc cbc ./middle.kbc "AAAA" "BBBB"
+./file-kbc.out ./middle.kbc dec cbc ./decrypt.txt "AAAA" "BBBB"
 
 ORIG_HASH=$(sha256sum original.txt | awk '{ print $1 }')
 DECR_HASH=$(sha256sum decrypt.txt | awk '{ print $1 }')
